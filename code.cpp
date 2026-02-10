325fa1985e1725065408ba0b7edb72fc94373e50
aryankhalati <aryankhalati9@gmail.com>
2026-02-10 21:09:35 +0530

    Video 3 complete

---
+++ code.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    cout<<"3+2"<<endl;
+    cout<<3+2<<endl;
+
+    //int 
+    int i = 9;
+    cout<<i<<endl;
+
+    //float 
+    float f = 1.23;
+    cout<<f<<endl;
+
+    //char 
+    char c = 's';
+    cout<<c<<endl;
+
+    //boolean
+    bool b = true;
+    cout<<b<<endl;
+
+    //double
+    double d = 1.23456;
+    cout<<d<<endl;
+     
+    //long
+    long l = 12334;
+    cout<<l<<endl;
+    return 0;
+    
+}
+
+
+
